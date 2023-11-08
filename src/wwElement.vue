b6dc6e98d8bcb796340678bc7c453034707c5544
Aurélie VIOLETTE <orwel.violette@gmail.com>
2023-11-08 17:31:41 +0100

    Add readonly and handle options

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,184 +1,194 @@
 <template>
-  <div class="ww-kanban" :style="kanbanStyle">
-    <template v-if="content.uncategorizedStack">
-      <wwLayoutItemContext :index="0" :item="null" :data="uncategorizedStack" is-repeat>
-        <wwElement 
-          v-bind="content.stackElement" 
-          :ww-props="{ ...stackConfig, items: uncategorizedStack.items, stack: null }"
-          class="ww-kanban-stack"
-        ></wwElement>
-      </wwLayoutItemContext>
-    </template>
+    <div class="ww-kanban" :style="kanbanStyle">
+        <template v-if="content.uncategorizedStack">
+            <wwLayoutItemContext :index="0" :item="null" :data="uncategorizedStack" is-repeat>
+                <wwElement
+                    v-bind="content.stackElement"
+                    :ww-props="{ ...stackConfig, items: uncategorizedStack.items, stack: null }"
+                    class="ww-kanban-stack"
+                ></wwElement>
+            </wwLayoutItemContext>
+        </template>
 
-    <template v-for="(stack, index) in internalStacks" :key="'ww-stack-' + index">
-      <wwLayoutItemContext :index="index" :item="null" is-repeat :data="stack" :repeated-items="internalStacks">
-        <wwElement 
-          v-bind="content.stackElement" 
-          :ww-props="{ ...stackConfig, items: stack.items, stack: stack.value }"
-          class="ww-kanban-stack"
-        ></wwElement>
-      </wwLayoutItemContext>
-    </template>
-  </div>
+        <template v-for="(stack, index) in internalStacks" :key="'ww-stack-' + index">
+            <wwLayoutItemContext :index="index" :item="null" is-repeat :data="stack" :repeated-items="internalStacks">
+                <wwElement
+                    v-bind="content.stackElement"
+                    :ww-props="{ ...stackConfig, items: stack.items, stack: stack.value }"
+                    class="ww-kanban-stack"
+                ></wwElement>
+            </wwLayoutItemContext>
+        </template>
+    </div>
 </template>
 
 <script>
-import { provide, reactive, ref, watch } from 'vue'
+import { provide, reactive, ref, watch } from "vue";
 
 export default {
-  props: {
-    content: { type: Object, required: true },
-    uid: { type: String, required: true },
-  },
-  emits: ['trigger-event', 'update:content:effect'],
-  setup(props, {emit}) {
-    const internalStacks = ref([])
-    const uncategorizedStack = reactive({
-      label: 'Uncategorized',
-      value: null,
-      items: []
-    })
-    
-    provide('customHandler', (change, {stack: stackValue, updatedStackItems}) => {
-      if (change.moved) {
-        emit('trigger-event', { 
-          name: 'item:moved', 
-          event: { 
-            item: change.moved.element,
-            from: stackValue,
-            to: stackValue,
-            oldIndex: change.moved.oldIndex,
-            newIndex: change.moved.newIndex,
-            updatedList: updatedStackItems
-          }
-        })
-      }
+    props: {
+        content: { type: Object, required: true },
+        uid: { type: String, required: true },
+    },
+    emits: ["trigger-event", "update:content:effect"],
+    setup(props, { emit }) {
+        const internalStacks = ref([]);
+        const uncategorizedStack = reactive({
+            label: "Uncategorized",
+            value: null,
+            items: [],
+        });
 
-      if (change.added) {
-        emit('trigger-event', {
-          name: 'item:moved',
-          event: {
-            item: change.added.element,
-            from: wwLib.resolveObjectPropertyPath(change.added.element, props.content.stackedBy),
-            to: stackValue,
-            oldIndex: null,
-            newIndex: change.added.newIndex,
-            updatedList: updatedStackItems
-          }
-        })
-      }
-    })
+        provide("customHandler", (change, { stack: stackValue, updatedStackItems }) => {
+            if (change.moved) {
+                emit("trigger-event", {
+                    name: "item:moved",
+                    event: {
+                        item: change.moved.element,
+                        from: stackValue,
+                        to: stackValue,
+                        oldIndex: change.moved.oldIndex,
+                        newIndex: change.moved.newIndex,
+                        updatedList: updatedStackItems,
+                    },
+                });
+            }
 
-    const isDraggingManager = reactive({})
-    provide('customDragHandler', (isDragging, {stack}) => isDraggingManager[stack] = isDragging)
-  
-    const { setValue: setDrag } = wwLib.wwVariable.useComponentVariable({
-      uid: props.uid,
-      name: 'isDragging',
-      type: 'boolean',
-      defaultValue: false,
-      readonly: true,
-    });
-    watch(isDraggingManager, (value) => {
-      setDrag(Object.values(value).some(isDragging => isDragging))
-    }, {deep: true})
+            if (change.added) {
+                emit("trigger-event", {
+                    name: "item:moved",
+                    event: {
+                        item: change.added.element,
+                        from: wwLib.resolveObjectPropertyPath(change.added.element, props.content.stackedBy),
+                        to: stackValue,
+                        oldIndex: null,
+                        newIndex: change.added.newIndex,
+                        updatedList: updatedStackItems,
+                    },
+                });
+            }
+        });
 
-    return { internalStacks, uncategorizedStack }
-  },
-  computed: {
-    stacks() {
-      const stacks = wwLib.wwCollection.getCollectionData(this.content.stacks)
-      if (!Array.isArray(stacks)) return []
-      return stacks
+        const isDraggingManager = reactive({});
+        provide("customDragHandler", (isDragging, { stack }) => (isDraggingManager[stack] = isDragging));
+
+        const { setValue: setDrag } = wwLib.wwVariable.useComponentVariable({
+            uid: props.uid,
+            name: "isDragging",
+            type: "boolean",
+            defaultValue: false,
+            readonly: true,
+        });
+        watch(
+            isDraggingManager,
+            (value) => {
+                setDrag(Object.values(value).some((isDragging) => isDragging));
+            },
+            { deep: true }
+        );
+
+        return { internalStacks, uncategorizedStack };
     },
-    items() {
-      const items = wwLib.wwCollection.getCollectionData(this.content.items)
-      if (!Array.isArray(items)) return []
-      return items
+    computed: {
+        stacks() {
+            const stacks = wwLib.wwCollection.getCollectionData(this.content.stacks);
+            if (!Array.isArray(stacks)) return [];
+            return stacks;
+        },
+        items() {
+            const items = wwLib.wwCollection.getCollectionData(this.content.items);
+            if (!Array.isArray(items)) return [];
+            return items;
+        },
+        stackConfig() {
+            return {
+                sortable: this.content.sortable,
+                group: "kanban-" + this.uid,
+                itemKey: this.content.itemKey,
+                handle: this.content.handle,
+                readonly: this.content.readonly,
+            };
+        },
+        kanbanStyle() {
+            return {
+                "--wrap-stacks": this.content.wrapStacks ? "wrap" : "nowrap",
+            };
+        },
     },
-    stackConfig() {
-      return {
-        sortable: this.content.sortable, 
-        group: 'kanban-' + this.uid,
-        itemKey: this.content.itemKey
-      }
+    watch: {
+        "content.stackValue"() {
+            this.refreshStacks();
+        },
+        "content.stackedBy"() {
+            this.refreshStacks();
+        },
+        "content.sortedBy"() {
+            this.refreshStacks();
+        },
+        "content.sortOrder"() {
+            this.refreshStacks();
+        },
+        stacks() {
+            this.refreshStacks();
+        },
+        items() {
+            this.refreshStacks();
+        },
     },
-    kanbanStyle() {
-      return {
-        '--wrap-stacks': this.content.wrapStacks ? 'wrap' : 'nowrap'
-      }
-    }
-  },
-  watch: {
-    'content.stackValue'() {
-      this.refreshStacks()
+    methods: {
+        refreshStacks() {
+            this.internalStacks = this.stacks
+                .map((stack) => ({
+                    label: wwLib.resolveObjectPropertyPath(stack, this.content.stackLabel || "label") || "",
+                    value: wwLib.resolveObjectPropertyPath(stack, this.content.stackValue || "value") || "",
+                }))
+                .map((stack) => ({
+                    ...stack,
+                    items: this.items
+                        .filter((item) => wwLib.resolveObjectPropertyPath(item, this.content.stackedBy) === stack.value)
+                        .sort((a, b) => {
+                            if (!this.content.sortedBy) return 0;
+                            const valueA = wwLib.resolveObjectPropertyPath(a, this.content.sortedBy);
+                            const valueB = wwLib.resolveObjectPropertyPath(b, this.content.sortedBy);
+                            if (this.content.sortOrder === "asc") {
+                                return valueA > valueB ? 1 : -1;
+                            } else {
+                                return valueA > valueB ? -1 : 1;
+                            }
+                        }),
+                }));
+            const stacksList = this.stacks.map((stack) =>
+                wwLib.resolveObjectPropertyPath(stack, this.content.stackValue || "value")
+            );
+            this.uncategorizedStack.items = this.items.filter(
+                (item) => !stacksList.includes(wwLib.resolveObjectPropertyPath(item, this.content.stackedBy))
+            );
+        },
+        /* wwEditor:start */
+        getTestEvent() {
+            if (!this.internalStacks.length) throw new Error("No stack found");
+            if (!this.items.length) throw new Error("No item found");
+            return {
+                item: this.items[0],
+                from: this.internalStacks[0].value,
+                to: this.internalStacks[0].value,
+                oldIndex: 0,
+                newIndex: 1,
+                updatedList: this.items,
+            };
+        },
+        /* wwEditor:end */
     },
-    'content.stackedBy'() {
-      this.refreshStacks()
+    created() {
+        this.refreshStacks();
     },
-    'content.sortedBy'() {
-      this.refreshStacks()
-    },
-    'content.sortOrder'() {
-      this.refreshStacks()
-    },
-    stacks() {
-      this.refreshStacks()
-    },
-    items() {
-      this.refreshStacks()
-    }
-  },
-  methods: {
-    refreshStacks() {
-      this.internalStacks = this.stacks
-          .map(stack => ({
-            label: wwLib.resolveObjectPropertyPath(stack, this.content.stackLabel || 'label') || '',
-            value: wwLib.resolveObjectPropertyPath(stack, this.content.stackValue || 'value') || '',
-          }))
-          .map(stack => ({
-            ...stack,
-            items: this.items
-              .filter(item => wwLib.resolveObjectPropertyPath(item, this.content.stackedBy) === stack.value)
-              .sort((a, b) => {
-                if(!this.content.sortedBy) return 0
-                const valueA = wwLib.resolveObjectPropertyPath(a, this.content.sortedBy)
-                const valueB = wwLib.resolveObjectPropertyPath(b, this.content.sortedBy)
-                if (this.content.sortOrder === 'asc') {
-                  return valueA > valueB ? 1 : -1
-                } else {
-                  return valueA > valueB ? -1 : 1
-                }
-              })
-          }))
-      const stacksList = this.stacks.map(stack => wwLib.resolveObjectPropertyPath(stack, this.content.stackValue || 'value'))
-      this.uncategorizedStack.items = this.items.filter(item => !stacksList.includes(wwLib.resolveObjectPropertyPath(item, this.content.stackedBy)))
-    },
-    /* wwEditor:start */
-    getTestEvent() {
-      if(!this.internalStacks.length) throw new Error('No stack found')
-      if(!this.items.length) throw new Error('No item found')
-      return {
-        item: this.items[0],
-        from: this.internalStacks[0].value,
-        to: this.internalStacks[0].value,
-        oldIndex: 0,
-        newIndex: 1,
-        updatedList: this.items
-      }
-    }
-    /* wwEditor:end */
-  },
-  created() {
-    this.refreshStacks()
-  }
 };
 </script>
 
 <style lang="scss" scoped>
 .ww-kanban {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: var(--wrap-stacks);
+    display: flex;
+    flex-direction: row;
+    flex-wrap: var(--wrap-stacks);
 }
 </style>
--- ww-config.js
+++ ww-config.js
@@ -1,192 +1,238 @@
-function showObjectPropertyPath(basePropertyKey, {content, boundProps}) {
-  return boundProps[basePropertyKey] && content[basePropertyKey] && typeof wwLib.wwCollection.getCollectionData(content[basePropertyKey])[0] === 'object'
+function showObjectPropertyPath(basePropertyKey, { content, boundProps }) {
+    return (
+        boundProps[basePropertyKey] &&
+        content[basePropertyKey] &&
+        typeof wwLib.wwCollection.getCollectionData(content[basePropertyKey])[0] === "object"
+    );
 }
-function getObjectPropertyPathOptions(basePropertyKey, {content}) {
-  const data = wwLib.wwCollection.getCollectionData(content[basePropertyKey])
-  if (!data.length || typeof data[0] !== 'object') {
-      return null;
-  }
+function getObjectPropertyPathOptions(basePropertyKey, { content }) {
+    const data = wwLib.wwCollection.getCollectionData(content[basePropertyKey]);
+    if (!data.length || typeof data[0] !== "object") {
+        return null;
+    }
 
-  return { object: data[0] };
+    return { object: data[0] };
 }
 
 export default {
-  editor: {
-    label: {
-      en: 'Kanban',
-    },
-    bubble: {
-      icon: 'template',
-    },
-    icon: 'template',
-    customSettingsPropertiesOrder: ['items', ['itemKey', 'stackedBy', 'sortedBy', 'sortOrder']]
-  },
-  triggerEvents: [
-    {
-      name: 'item:moved',
-      label: { en: 'On item moved' },
-      event: {
-        item: {},
-        from: '',
-        to: '',
-        oldIndex: 0,
-        newIndex: 1,
-        updatedList: []
-      },
-      getTestEvent: 'getTestEvent',
-      default: true
-    },
-  ],
-  properties: {
-    stackElement: {
-      hidden: true,
-      //ww-stack
-      defaultValue: { isWwObject: true, type: '5a88036f-22ea-4f8d-b4a5-bc226ef95061' },
-    },
-    wrapStacks: {
-      label: {
-        en: 'Wrap stacks',
-      },
-      type: 'OnOff',
-      defaultValue: true,
-      responsive: true,
-      bindable: true,
-      states: true,
-      classes: true,
-    },
-    items: {
-      label: {
-        en: 'Items',
-      },
-      type: 'Info',
-      options: {
-        text: { en: 'Bind your data' }
-      },
-      bindable: true,
-      defaultValue: [],
-      section: 'settings',
-    },
-    itemKey: {
-      hidden: (content, sidepanelContent, boundProps) => !showObjectPropertyPath('items', {content, boundProps}),
-      label: {
-          en: 'Item key',
-      },
-      type: 'ObjectPropertyPath',
-      options: content => getObjectPropertyPathOptions('items', {content}),
-      defaultValue: null,
-      section: 'settings',
-    },
-    stackedBy: {
-      hidden: (content, sidepanelContent, boundProps) => !showObjectPropertyPath('items', {content, boundProps}),
-      label: {
-          en: 'Stacked by',
-      },
-      type: 'ObjectPropertyPath',
-      options: content => getObjectPropertyPathOptions('items', {content}),
-      defaultValue: null,
-      section: 'settings',
-    },
-    sortedBy: {
-      hidden: (content, sidepanelContent, boundProps) => !showObjectPropertyPath('items', {content, boundProps}),
-      label: {
-          en: 'Sorted by',
-      },
-      type: 'ObjectPropertyPath',
-      options: content => getObjectPropertyPathOptions('items', {content}),
-      defaultValue: null,
-      section: 'settings',
-    },
-    sortOrder: {
-      hidden: (content, sidepanelContent, boundProps) => !showObjectPropertyPath('items', {content, boundProps}) || !content.sortedBy,
-      label: {
-        en: 'Sort order'
-      },
-      type: 'TextRadioGroup',
-      options: {
-        choices: [
-          {
-            label: { en: 'Asc' },
-            value: 'asc',
-            default: true,
-          },
-          {
-            label: { en: 'Desc' },
-            value: 'desc',
-          },
-        ]
-      },
-      defaultValue: 'asc',
-      bindable: true,
-      section: 'settings'
-    },
-    sortable: {
-      hidden: (content, sidepanelContent, boundProps) => !showObjectPropertyPath('items', {content, boundProps}),
-      label: {
-        en: 'Sortable',
-      },
-      type: 'OnOff',
-      defaultValue: true,
-      section: 'settings',
-    },
-    uncategorizedStack: {
-      label: {
-        en: 'Uncategorized stack',
-      },
-      type: 'OnOff',
-      defaultValue: false,
-      section: 'settings'
-    },
-    stacks: {
-      label: {
-        en: 'Stacks',
-      },
-      type: 'Array',
-      bindable: true,
-      options: {
-        movable: true,
-        expandable: true,
-        getItemLabel(_, index) {
-          return `Stack ${index + 1}`;
+    editor: {
+        label: {
+            en: "Kanban",
         },
-        item: {
-          type: 'Object',
-          defaultValue: {label: '', value: ''},
-          options: {
-            item: {
-                label: {
-                    label: { en: 'Label' },
-                    type: 'Text',
+        bubble: {
+            icon: "template",
+        },
+        icon: "template",
+        customSettingsPropertiesOrder: [
+            "items",
+            ["itemKey", "stackedBy", "sortedBy", "sortOrder"],
+            "readonly",
+            "handle",
+        ],
+    },
+    triggerEvents: [
+        {
+            name: "item:moved",
+            label: { en: "On item moved" },
+            event: {
+                item: {},
+                from: "",
+                to: "",
+                oldIndex: 0,
+                newIndex: 1,
+                updatedList: [],
+            },
+            getTestEvent: "getTestEvent",
+            default: true,
+        },
+    ],
+    properties: {
+        stackElement: {
+            hidden: true,
+            //ww-stack
+            defaultValue: { isWwObject: true, type: "5a88036f-22ea-4f8d-b4a5-bc226ef95061" },
+        },
+        wrapStacks: {
+            label: {
+                en: "Wrap stacks",
+            },
+            type: "OnOff",
+            defaultValue: true,
+            responsive: true,
+            bindable: true,
+            states: true,
+            classes: true,
+        },
+        items: {
+            label: {
+                en: "Items",
+            },
+            type: "Info",
+            options: {
+                text: { en: "Bind your data" },
+            },
+            bindable: true,
+            defaultValue: [],
+            section: "settings",
+        },
+        itemKey: {
+            hidden: (content, sidepanelContent, boundProps) =>
+                !showObjectPropertyPath("items", { content, boundProps }),
+            label: {
+                en: "Item key",
+            },
+            type: "ObjectPropertyPath",
+            options: (content) => getObjectPropertyPathOptions("items", { content }),
+            defaultValue: null,
+            section: "settings",
+        },
+        stackedBy: {
+            hidden: (content, sidepanelContent, boundProps) =>
+                !showObjectPropertyPath("items", { content, boundProps }),
+            label: {
+                en: "Stacked by",
+            },
+            type: "ObjectPropertyPath",
+            options: (content) => getObjectPropertyPathOptions("items", { content }),
+            defaultValue: null,
+            section: "settings",
+        },
+        sortedBy: {
+            hidden: (content, sidepanelContent, boundProps) =>
+                !showObjectPropertyPath("items", { content, boundProps }),
+            label: {
+                en: "Sorted by",
+            },
+            type: "ObjectPropertyPath",
+            options: (content) => getObjectPropertyPathOptions("items", { content }),
+            defaultValue: null,
+            section: "settings",
+        },
+        sortOrder: {
+            hidden: (content, sidepanelContent, boundProps) =>
+                !showObjectPropertyPath("items", { content, boundProps }) || !content.sortedBy,
+            label: {
+                en: "Sort order",
+            },
+            type: "TextRadioGroup",
+            options: {
+                choices: [
+                    {
+                        label: { en: "Asc" },
+                        value: "asc",
+                        default: true,
+                    },
+                    {
+                        label: { en: "Desc" },
+                        value: "desc",
+                    },
+                ],
+            },
+            defaultValue: "asc",
+            bindable: true,
+            section: "settings",
+        },
+        sortable: {
+            hidden: (content, sidepanelContent, boundProps) =>
+                !showObjectPropertyPath("items", { content, boundProps }),
+            label: {
+                en: "Sortable",
+            },
+            type: "OnOff",
+            defaultValue: true,
+            section: "settings",
+        },
+        uncategorizedStack: {
+            label: {
+                en: "Uncategorized stack",
+            },
+            type: "OnOff",
+            defaultValue: false,
+            section: "settings",
+        },
+        stacks: {
+            label: {
+                en: "Stacks",
+            },
+            type: "Array",
+            bindable: true,
+            options: {
+                movable: true,
+                expandable: true,
+                getItemLabel(_, index) {
+                    return `Stack ${index + 1}`;
                 },
-                value: {
-                  label: { en: 'Value' },
-                  type: 'Text'
+                item: {
+                    type: "Object",
+                    defaultValue: { label: "", value: "" },
+                    options: {
+                        item: {
+                            label: {
+                                label: { en: "Label" },
+                                type: "Text",
+                            },
+                            value: {
+                                label: { en: "Value" },
+                                type: "Text",
+                            },
+                        },
+                    },
                 },
             },
-          },
+            defaultValue: [
+                { label: "Todo", value: "todo" },
+                { label: "In progress", value: "wip" },
+                { label: "Done", value: "done" },
+            ],
+            section: "settings",
         },
-      },
-      defaultValue: [{label: 'Todo', value: 'todo'}, {label: 'In progress', value: 'wip'}, {label: 'Done', value: 'done'}],
-      section: 'settings',
+        stackLabel: {
+            hidden: (content, sidepanelContent, boundProps) =>
+                !showObjectPropertyPath("stacks", { content, boundProps }),
+            label: {
+                en: "Label",
+            },
+            type: "ObjectPropertyPath",
+            options: (content) => getObjectPropertyPathOptions("stacks", { content }),
+            defaultValue: null,
+            section: "settings",
+        },
+        stackValue: {
+            hidden: (content, sidepanelContent, boundProps) =>
+                !showObjectPropertyPath("stacks", { content, boundProps }),
+            label: {
+                en: "Value",
+            },
+            type: "ObjectPropertyPath",
+            options: (content) => getObjectPropertyPathOptions("stacks", { content }),
+            defaultValue: null,
+            section: "settings",
+        },
+        readonly: {
+            label: { en: "Read only", fr: "Lecture seule" },
+            type: "OnOff",
+            section: "settings",
+            bindable: true,
+            defaultValue: false,
+            hidden: (content, sidePanelContent, boundProps, wwProps) => !!(wwProps && wwProps.readonly !== undefined),
+            /* wwEditor:start */
+            bindingValidation: {
+                type: "boolean",
+                tooltip: "A boolean that defines if the input is in readonly: `true | false`",
+            },
+            /* wwEditor:end */
+        },
+        handle: {
+            label: "Handle class",
+            type: "Text",
+            bindable: true,
+            section: "settings",
+            /* wwEditor:start */
+            bindingValidation: {
+                type: "string",
+                tooltip: "A string that represent the class of the handle",
+            },
+            /* wwEditor:end */
+        },
     },
-    stackLabel: {
-      hidden: (content, sidepanelContent, boundProps) => !showObjectPropertyPath('stacks', {content, boundProps}),
-      label: {
-          en: 'Label',
-      },
-      type: 'ObjectPropertyPath',
-      options: content => getObjectPropertyPathOptions('stacks', {content}),
-      defaultValue: null,
-      section: 'settings',
-    },
-    stackValue: {
-      hidden: (content, sidepanelContent, boundProps) => !showObjectPropertyPath('stacks', {content, boundProps}),
-      label: {
-          en: 'Value',
-      },
-      type: 'ObjectPropertyPath',
-      options: content => getObjectPropertyPathOptions('stacks', {content}),
-      defaultValue: null,
-      section: 'settings',
-    },
-  },
 };
